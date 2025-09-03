7a8ecdcbca0b82209fc4e65edc6f5aa062e104ac
Oluwateniyomi <teniopemipo@gmail.com>
2025-09-03 15:09:30 +0100

    Update ESLint and Prettier configurations to support TypeScript and enhance formatting options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,7 @@
 import js from "@eslint/js";
+import * as tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import configPrettier from "eslint-config-prettier";
 
 export default [
   {
@@ -10,8 +13,22 @@ export default [
       ecmaVersion: "latest",
       sourceType: "module",
     },
-    plugins: {
-    },
+    plugins: {},
     rules: {},
   },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.mts", "**/*.cts"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-undef": "off",
+    },
+  },
+  configPrettier,
 ]; 
